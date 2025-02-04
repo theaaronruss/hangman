8de540e1ed2245c19c12a8ef388904bbbcfec959
Aaron Russell <158230700+theaaronruss@users.noreply.github.com>
2025-02-04 01:14:34 -0500

    Add hangman graphics

--- main.cpp
+++ main.cpp
@@ -1,7 +1,56 @@
-#include <iostream>
+#include <array>
+#include <string>
+
+std::array<std::string, 5> hangman = {
+    // 1 mistake
+    " *--*\n"
+    " |  |\n"
+    "    |\n"
+    "    |\n"
+    "    |\n"
+    "    |\n"
+    "    |\n"
+    "-----\n",
+    // 2 mistake
+    " *--*\n"
+    " |  |\n"
+    " 0  |\n"
+    "    |\n"
+    "    |\n"
+    "    |\n"
+    "    |\n"
+    "-----\n",
+    // 3 mistake
+    " *--*\n"
+    " |  |\n"
+    " 0  |\n"
+    " |  |\n"
+    " |  |\n"
+    "    |\n"
+    "    |\n"
+    "-----\n",
+    // 4 mistake
+    " *--*\n"
+    " |  |\n"
+    " 0  |\n"
+    "/|\\ |\n"
+    " |  |\n"
+    "    |\n"
+    "    |\n"
+    "-----\n",
+    // 5 mistake
+    " *--*\n"
+    " |  |\n"
+    " 0  |\n"
+    "/|\\ |\n"
+    " |  |\n"
+    "/ \\ |\n"
+    "    |\n"
+    "-----\n"
+};
 
 int main()
 {
-    std::cout << "Hello, World!\n";
+    // TODO: Implement game
     return 0;
 }
