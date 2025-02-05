1234f76e102ed1b4d22aba51cf526d23361fe0bf
Aaron Russell <158230700+theaaronruss@users.noreply.github.com>
2025-02-05 00:08:13 -0500

    Add function to retrieve guesses from player

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,39 @@
+#include "../include/phrases.h"
+#include <cstdio>
+#include <cctype>
+#include <iostream>
+#include <regex>
+
+char getGuess()
+{
+    bool valid { false };
+    char guess = ' ';
+    while (!valid)
+    {
+        std::cout << "Enter your guess: ";
+        guess = getchar();
+        std::fflush(stdin);
+        valid = std::regex_match(std::string(1, guess), std::regex("[a-zA-Z]"));
+        if (!valid)
+        {
+            std::cout << "Invalid input! Please enter a letter!\n";
+        }
+    }
+    return std::toupper(guess);
+}
+
 int main()
 {
-    // TODO: Implement game
+    std::string phrase = Phrases::getPhrase(4);
+    std::string guesses;
+    int mistakes = 0;
+    while (mistakes < 5)
+    {
+        Phrases::printPhrase(phrase, guesses);
+        char guess = getGuess();
+        guesses.push_back(guess);
+        mistakes++;
+        std::cout << "\n";
+    }
     return 0;
 }
